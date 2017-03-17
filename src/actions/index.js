88bbd27322d7fa80f59434e6d54ed398014d6e40
Crysknife <ad.dolorem@yandex.ru>
2017-03-17 18:30:37 +0300

    Forgot to remove commended code

--- src/actions/index.js
+++ src/actions/index.js
@@ -47,48 +47,48 @@ export const logOut = (token) => dispatch => {
 
 export const checkUser = (user) => dispatch => {
   
-  //Mock response, because fuck cors!
-  setTimeout(() => {
-    if (user.eMail === 'admin@mail.ru' && user.password === 'admin') {
-      let response = {
-        data: {
-          role_name: 'isadmin',
-          token: '4f5bd8223d51368e026'
-        }
-      };
-      dispatch({ type: 'CHECK_USER_SUCCESS', payload: {
-        eMail: user.eMail,
-        roleName: response.data.role_name,
-        token: response.data.token
-      }});
-    } else if (user.eMail === '500@mail.ru') {
-      let response = {
-        status: 500
-      };
-      dispatch({ type: 'CHECK_USER_FAILURE', payload: {
-        response: response.status
-      }});
-    } else {
-      let response = {
-        status: 401
-      };
-      dispatch({ type: 'CHECK_USER_FAILURE', payload: {
-        response: response.status
-      }});
-    }
-  }, 1500);
-
-  // axios.post('core/login', user)
-  //   .then(function (response) {
+  // //Mock response, because fuck cors!
+  // setTimeout(() => {
+  //   if (user.eMail === 'admin@mail.ru' && user.password === 'admin') {
+  //     let response = {
+  //       data: {
+  //         role_name: 'isadmin',
+  //         token: '4f5bd8223d51368e026'
+  //       }
+  //     };
   //     dispatch({ type: 'CHECK_USER_SUCCESS', payload: {
   //       eMail: user.eMail,
   //       roleName: response.data.role_name,
   //       token: response.data.token
   //     }});
-  //   })
-  //   .catch(function (error) {
+  //   } else if (user.eMail === '500@mail.ru') {
+  //     let response = {
+  //       status: 500
+  //     };
   //     dispatch({ type: 'CHECK_USER_FAILURE', payload: {
-  //       response: error.response.status
+  //       response: response.status
   //     }});
-  //   });
+  //   } else {
+  //     let response = {
+  //       status: 401
+  //     };
+  //     dispatch({ type: 'CHECK_USER_FAILURE', payload: {
+  //       response: response.status
+  //     }});
+  //   }
+  // }, 1500);
+
+  axios.post('core/login', user)
+    .then(function (response) {
+      dispatch({ type: 'CHECK_USER_SUCCESS', payload: {
+        eMail: user.eMail,
+        roleName: response.data.role_name,
+        token: response.data.token
+      }});
+    })
+    .catch(function (error) {
+      dispatch({ type: 'CHECK_USER_FAILURE', payload: {
+        response: error.response.status
+      }});
+    });
 };
