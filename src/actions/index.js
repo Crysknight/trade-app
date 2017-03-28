67967452b270d5890b91eb0bb5bd6c666db8f4d4
Crysknife <ad.dolorem@yandex.ru>
2017-03-28 10:19:30 +0400

    Training

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,7 +7,6 @@ export const tryLoginAgain = () => {
 }
 
 /* Async block */
-/* Added something */
 
 export const cancelOrders = (token, orders) => dispatch => {
   axios.post('core/deleteordersarray', { token, ids: orders })
